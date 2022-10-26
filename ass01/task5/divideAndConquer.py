f1234c2f596c7de090d9de8ed9abf052be6b640e
Mustafa Kamal <mustfakmalik@gmail.com>
2022-10-26 13:48:48 +0500

    task5-dnc modified and corrected

--- ass01/task5/divideAndConquer.py
+++ ass01/task5/divideAndConquer.py
@@ -1,13 +1,14 @@
 def foo(L, p, r):
     if (p==r):
             return L[p]
-      
+    if(r+1== p):
+            return 1 
     
     
     else:
         mid = (p+r)//2
         leftmid = (p +mid)//2
-        rightmid = (mid + 1 + r)//2
+        rightmid = (mid+ r)//2
 
         first = foo(L, p, leftmid)
         second = foo(L, leftmid+1, mid)
@@ -17,4 +18,4 @@ def foo(L, p, r):
     return first * second * third * fourth
 
 
-print(foo([3,4,5,7], 0, 3))
+print(foo([3,4,5,7,7,5,3,2,9,1], 0, 9))
